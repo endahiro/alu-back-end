117bb83c9d8f4481b6b2bd3e639ca2e1fb3898ff
Evan Ndahiro <evanndahiro@gmail.com>
2025-12-04 06:19:54 -0500

    Adjust task 0 implementation to canonical style

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
+    employee_id = sys.argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com"
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     ).json()
 
     total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed") is True]
+    done_tasks = [task for task in todos if task.get("completed")]
 
     print(
         "Employee {} is done with tasks({}/{}):".format(
